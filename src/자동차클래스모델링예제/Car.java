9cfd3de70daccfb611d7527348563a90b3b720c5
JongIn Yoon <bellpp93@gmail.com>
2022-01-15 23:04:05 +0900

    Not used line 수정

--- src/자동차클래스모델링예제/Car.java
+++ src/자동차클래스모델링예제/Car.java
@@ -3,7 +3,7 @@ package 자동차클래스모델링예제;
 public class Car {
 //	객체가 가져야할 속성 => 멤버 변수
 	private int speed;       //현재 속도
-	private int wheelNum;    //바퀴의 수
+//	private int wheelNum;    //바퀴의 수
 	private String carName;  //자동차 이름
 	
 //	생성자1
